ede65c9c3bf9b616fec2283776377b5f36fb0d18
Marcin Środa <marcinsroda94@gmail.com>
2019-04-01 01:08:07 +0200

    Wallrunning.

--- Source/FPPlatformer/Characters/Player/MyCharacter.cpp
+++ Source/FPPlatformer/Characters/Player/MyCharacter.cpp
@@ -4,6 +4,8 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "Runtime/Engine/Classes/GameFramework/CharacterMovementComponent.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
@@ -23,13 +25,19 @@ AMyCharacter::AMyCharacter()
 	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
 	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+	
+	LineTraceStart = FVector(0.0f, 0.0f, -50.0f);
+
+	JumpCount = 0;
+
+	IsWallRunning = false;
+	CanWallRun = true;
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
 }
 
 // Called every frame
@@ -37,6 +45,79 @@ void AMyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (IsWallRunning)
+	{
+		//check if player wants to stop wallrunning
+		if (!ParkourKeyDown)
+		{
+			StopWallrun();
+		}
+		//check if the wallrunning needs to be stopped by the gravity
+		else if ((WallNeighborhood == WN_Front && GetVelocity().Z < MinVerticalZSpeed) || (WallNeighborhood == WN_Side && GetVelocity().Z < MinHorizontalZSpeed))
+		{
+			StopWallrun();
+		}
+		//check if there's still a wall to run on
+		else
+		{
+			const FRotator rot(0.0f, 0.0f, 90.0f);
+			FVector startPosShift = rot.RotateVector(AttachedWallNormal) * LineTraceShift;
+
+			FHitResult outHit;
+			bool isHit = false;
+
+			FVector startLocation = GetActorLocation() + LineTraceStart + startPosShift;
+			FCollisionQueryParams collisionParams;
+
+			//1st check
+			isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation - AttachedWallNormal * LineTraceRange, ECC_Visibility, collisionParams);
+
+			if (isHit)
+			{
+				AttachedWallNormal = outHit.ImpactNormal;
+				DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+			}
+			//2nd check
+			else
+			{
+				startLocation -= 2 * startPosShift;
+				isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation - AttachedWallNormal * LineTraceRange, ECC_Visibility, collisionParams);
+
+				if (isHit)
+				{
+					AttachedWallNormal = outHit.ImpactNormal;
+					DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+				}
+				else
+				{
+					StopWallrun();
+				}
+			}
+		}
+	}
+	else
+	{
+		WallNeighborhood = CheckForWallsNearby(AttachedWallNormal);
+
+		//if player wants to wallrun and there's a wall nearby, that is vertical
+		if (ParkourKeyDown && CanWallRun && WallNeighborhood != WN_None && AttachedWallNormal.Z >= -0.05f && AttachedWallNormal.Z < 0.15f)
+		{
+			CanWallRun = false;			
+			IsWallRunning = true;
+			//wall is in front of the player - run upwards
+			if (WallNeighborhood == WN_Front)
+			{
+				GetCharacterMovement()->GravityScale = VerticalParkourGravityScale;
+				LaunchCharacter(FVector(0.0f, 0.0f, VerticalWallRunForce) - AttachedWallNormal * WallStickForce, true, true);
+			}
+			//wall on the side
+			else
+			{
+				GetCharacterMovement()->GravityScale = HorizontalParkourGravityScale;
+				LaunchCharacter(FVector::VectorPlaneProject(FirstPersonCameraComponent->GetForwardVector(), AttachedWallNormal).GetUnsafeNormal2D() * HorizontalWallRunForce - AttachedWallNormal * WallStickForce, true, true);
+			}
+		}
+	}
 }
 
 // Called to bind functionality to input
@@ -49,23 +130,26 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// Bind fire event
-	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AMyCharacter::OnFire);
+	PlayerInputComponent->BindAction("Parkour", IE_Pressed, this, &AMyCharacter::ParkourStart);
+	PlayerInputComponent->BindAction("Parkour", IE_Released, this, &AMyCharacter::ParkourEnd);
 
-	// Bind fire event
-	PlayerInputComponent->BindAction("Fire2", IE_Pressed, this, &AMyCharacter::OnAltFire);
+// Bind fire event
+PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AMyCharacter::OnFire);
 
-	// Bind movement events
-	PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+// Bind fire event
+PlayerInputComponent->BindAction("Fire2", IE_Pressed, this, &AMyCharacter::OnAltFire);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AMyCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AMyCharacter::LookUpAtRate);
+// Bind movement events
+PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+
+// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+// "turn" handles devices that provide an absolute delta, such as a mouse.
+// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+PlayerInputComponent->BindAxis("TurnRate", this, &AMyCharacter::TurnAtRate);
+PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+PlayerInputComponent->BindAxis("LookUpRate", this, &AMyCharacter::LookUpAtRate);
 
 }
 
@@ -108,9 +192,75 @@ void AMyCharacter::OnAltFire()
 	}
 }
 
+void AMyCharacter::ParkourStart()
+{
+	ParkourKeyDown = true;
+}
+
+void AMyCharacter::ParkourEnd()
+{
+	ParkourKeyDown = false;
+}
+
+void AMyCharacter::Jump()
+{
+	if (IsWallRunning)
+	{
+		//walljump
+		FVector desiredVelocity = AttachedWallNormal * WallJumpForce;
+		//desiredVelocity.Z = GetCharacterMovement()->JumpZVelocity;
+		LaunchCharacter(desiredVelocity, true, true);
+		//StopWallrun();
+		JumpCount = 1;
+		CanWallRun = true;
+	}
+	else
+	{
+		// normal jump
+		if (JumpCount == 0)
+		{
+			Super::Jump();
+			JumpCount++;
+		}
+		// second jump (air launch)
+		else if (JumpCount < JumpLimit)
+		{
+			// desired air launch velocity based on the input
+			FVector desiredVelocity = FVector(InputComponent->GetAxisValue(TEXT("MoveForward")), InputComponent->GetAxisValue(TEXT("MoveRight")), 0.0f);
+
+			// if there's input from player, launch him in the desired direction, making sure he doesn't lose too much of the current velocity
+			if (desiredVelocity.Size() > 0.0f)
+			{
+				desiredVelocity = GetActorRotation().RotateVector(desiredVelocity.GetUnsafeNormal2D());
+
+				float dotProduct = FVector::DotProduct(desiredVelocity, GetVelocity().GetUnsafeNormal2D());
+
+				float horizontalSpeed = FMath::Max(DoubleJumpForce, GetVelocity().Size2D() * dotProduct);
+				float verticalSpeed = FMath::Max(GetCharacterMovement()->JumpZVelocity, GetVelocity().Z * dotProduct);
+
+				desiredVelocity *= horizontalSpeed;
+				desiredVelocity.Z = verticalSpeed;
+
+				LaunchCharacter(desiredVelocity, true, true);
+				JumpCount++;
+			}
+			// else don't change his horizontal velocity
+			else
+			{
+				// if launch can affect vertical speed
+				if (GetCharacterMovement()->JumpZVelocity > GetVelocity().Z)
+				{
+					LaunchCharacter(FVector(0.0f, 0.0f, GetCharacterMovement()->JumpZVelocity), false, true);
+					JumpCount++;
+				}
+			}
+		}
+	}
+}
+
 void AMyCharacter::MoveForward(float Value)
 {
-	if (Value != 0.0f)
+	if (Value != 0.0f && !IsWallRunning)
 	{
 		// add movement in that direction
 		AddMovementInput(GetActorForwardVector(), Value);
@@ -119,7 +269,7 @@ void AMyCharacter::MoveForward(float Value)
 
 void AMyCharacter::MoveRight(float Value)
 {
-	if (Value != 0.0f)
+	if (Value != 0.0f && !IsWallRunning)
 	{
 		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
@@ -136,4 +286,78 @@ void AMyCharacter::LookUpAtRate(float Rate)
 {
 	// calculate delta for this frame from the rate information
 	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMyCharacter::Landed(const FHitResult & Hit)
+{
+	Super::Landed(Hit);
+	JumpCount = 0;
+	CanWallRun = true;
+	IsWallRunning = false;
+}
+
+EWallNeighborhood AMyCharacter::CheckForWallsNearby(FVector &wallNormal)
+{
+	FHitResult outHit;
+	bool isHit = false;
+
+	FVector startLocation = GetActorLocation() + LineTraceStart;
+
+	FCollisionQueryParams collisionParams;	
+
+	// front
+	isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation + GetActorForwardVector() * (LineTraceRange + LineTraceShift), ECC_Visibility, collisionParams);
+
+	if (isHit)
+	{
+		wallNormal = outHit.ImpactNormal;
+		return WN_Front;
+	}
+
+	//right side 1st check
+	startLocation.X += LineTraceShift;
+	isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation + GetActorForwardVector() * LineTraceRange, ECC_Visibility, collisionParams);
+	if (isHit)
+	{
+		DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+		wallNormal = outHit.ImpactNormal;
+		return WN_Side;
+	}
+
+	//right side 2nd check
+	startLocation.X -= 2 * LineTraceShift;
+	isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation + GetActorForwardVector() * LineTraceRange, ECC_Visibility, collisionParams);
+	if (isHit)
+	{
+		DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+		wallNormal = outHit.ImpactNormal;
+		return WN_Side;
+	}
+
+	//left side 1st check
+	isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation - GetActorForwardVector() * LineTraceRange, ECC_Visibility, collisionParams);
+	if (isHit)
+	{
+		DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+		wallNormal = outHit.ImpactNormal;
+		return WN_Side;
+	}
+
+	//left side 2nd check
+	startLocation.X += 2 * LineTraceShift;
+	isHit = GetWorld()->LineTraceSingleByChannel(outHit, startLocation, startLocation - GetActorForwardVector() * LineTraceRange, ECC_Visibility, collisionParams);
+	if (isHit)
+	{
+		DrawDebugLine(GetWorld(), outHit.ImpactPoint, outHit.ImpactPoint + outHit.ImpactNormal * LineTraceRange, FColor::Green, false, 1, 0, 1);
+		wallNormal = outHit.ImpactNormal;
+		return WN_Side;
+	}
+
+	return WN_None;
+}
+
+void AMyCharacter::StopWallrun()
+{
+	IsWallRunning = false;
+	GetCharacterMovement()->GravityScale = 1.0f;
 }
--- Source/FPPlatformer/Characters/Player/MyCharacter.h
+++ Source/FPPlatformer/Characters/Player/MyCharacter.h
@@ -6,6 +6,14 @@
 #include "GameFramework/Character.h"
 #include "MyCharacter.generated.h"
 
+UENUM()
+enum EWallNeighborhood
+{
+	WN_None,
+	WN_Front,
+	WN_Side
+};
+
 UCLASS()
 class FPPLATFORMER_API AMyCharacter : public ACharacter
 {
@@ -20,14 +28,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	/** Primary gun action. */
+	/** Primary gun action */
 	void OnFire();
 
-	/** Secondary gun action. */
+	/** Secondary gun action */
 	void OnAltFire();
+
+	/** Start parkour input */
+	void ParkourStart();
+
+	/** Stop parkour input */
+	void ParkourEnd();
 
 	/** Resets HMD orientation and position in VR. */
 	void OnResetVR();
+
+	/** Handles jumping and parkour */
+	void CustomJump();
 
 	/** Handles moving forward/backward */
 	void MoveForward(float Val);
@@ -59,17 +76,103 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* FirstPersonCameraComponent;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	/** Gun local position */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gun)
 		FVector GunOffset;
+
+	/** How many additional jumps can player perform airborne*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		int JumpLimit = 2;
+
+	/** Horizontal speed applied to player when performing double jump*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float DoubleJumpForce = 150;
+
+	/** Line trace start location for wall detection*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		FVector LineTraceStart;
+
+	/** Line trace range for wall detection*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float LineTraceRange = 40;
+
+	/** Line trace start location offset for additional wall detection*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float LineTraceShift = 30;
+
+	/** Gravity scale during vertical wallrunning*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float VerticalParkourGravityScale = 0.15f;
+
+	/** Gravity scale during horizontal wallrunning*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float HorizontalParkourGravityScale = 0.1f;
+
+	/** Force applied to player when beginning vertical wallrun*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float VerticalWallRunForce = 200.0f;
+
+	/** Force applied to player when beginning horizontal wallrun*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float HorizontalWallRunForce = 350.0f;
+
+	/** Force applied to player when beginning vertical wallrun*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float WallStickForce = 250.0f;
+
+	/** Minimum Z velocity for the player to keep wallrunning vertically*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float MinVerticalZSpeed = 0.0f;
+
+	/** Minimum Z velocity for the player to keep wallrunning horizontally*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float MinHorizontalZSpeed = -100.0f;
+
+	/** Velocity applied to the player on walljump*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parkour)
+		float WallJumpForce = 500.0f;
 
 	/** Gun class */
 	UPROPERTY(EditDefaultsOnly, Category = Projectile)
 		TSubclassOf<class AActor> GunClass;
 
-	// Called every frame
+	/** Called every frame */
 	virtual void Tick(float DeltaTime) override;
+
+	/** Handles jumping */
+	virtual void Jump() override;
+
+	/** Handles landing */
+	virtual void Landed(const FHitResult & Hit) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	/** Double jumps count */
+	int	JumpCount;
+
+	/** If the player stands close to a wall */
+	bool IsCloseToWall;
+
+	/** If player holds parkour key down */
+	bool ParkourKeyDown;
+
+	/** Can player start wallrunning during current jump */
+	bool CanWallRun;
+
+	/** If the player is wallrunning */
+	bool IsWallRunning;
+
+	/** Normal of the wall the player is close to/running on */
+	FVector AttachedWallNormal;
+
+	/** Status of the close walls */
+	EWallNeighborhood WallNeighborhood;
+
+	/** Search for walls  */
+	EWallNeighborhood CheckForWallsNearby(FVector &wallNormal);
+
+	/** Performed when player stops wallrunning*/
+	void StopWallrun();
 };
